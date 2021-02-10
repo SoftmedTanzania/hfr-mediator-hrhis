f54d95f8878d3fe62ecb4beba431699ceffb959c
cozej4 <ilakozejumanne@gmail.com>
2021-02-10 15:25:01 +0300

    Fixed codacy issues

--- src/main/java/tz/go/moh/him/hfr/mediator/orchestrator/DefaultOrchestrator.java
+++ src/main/java/tz/go/moh/him/hfr/mediator/orchestrator/DefaultOrchestrator.java
@@ -1,15 +1,12 @@
 package tz.go.moh.him.hfr.mediator.orchestrator;
 
 import akka.actor.UntypedActor;
-import akka.event.Logging;
-import akka.event.LoggingAdapter;
 import org.apache.http.HttpStatus;
 import org.openhim.mediator.engine.MediatorConfig;
 import org.openhim.mediator.engine.messages.FinishRequest;
 import org.openhim.mediator.engine.messages.MediatorHTTPRequest;
 
 public class DefaultOrchestrator extends UntypedActor {
-    LoggingAdapter log = Logging.getLogger(getContext().system(), this);
 
     private final MediatorConfig config;
 
